2fba1c045e6a06e8deed74b87efce9c76bfa11b6
Tymoteusz Buczkowski <moyubori@gmail.com>
2017-04-04 14:29:01 +0200

    Implement FPS/dt counter

---
+++ PerformanceTester/Assets/FPSCounter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+
+	// interval between fps checks given in seconds
+	public float updateInterval = 1f;
+
+	// reference to UI label
+	private Text counterLabel;
+	private int framesSinceLastCheck;
+	private float calculatedFPS;
+
+	void Awake(){
+		framesSinceLastCheck = 0;
+		calculatedFPS = 0;
+	}
+
+	void Start(){
+		counterLabel = transform.GetComponentInChildren<Text> ();
+		if (counterLabel == null) {
+			Debug.LogError ("FPSCounter label not found");
+		} else {
+			InvokeRepeating ("CalculateFPS", 0, updateInterval);
+		}
+	}
+
+	void Update(){
+		framesSinceLastCheck++;
+		UpdateCounterLabel ();
+	}
+
+	void CalculateFPS(){
+		calculatedFPS = framesSinceLastCheck / updateInterval;
+		framesSinceLastCheck = 0;
+	}
+
+	void UpdateCounterLabel(){
+		counterLabel.text = "FPS:" + calculatedFPS.ToString() + " dt:" + Time.deltaTime.ToString();
+	}
+
+}
