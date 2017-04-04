7090f6aeefa3485a52b828d9f0fa37abc5f7963c
Tymoteusz Buczkowski <moyubori@gmail.com>
2017-04-04 19:02:50 +0200

    Improved FPS/DT counters

--- PerformanceTester/Assets/FPSCounter.cs
+++ PerformanceTester/Assets/FPSCounter.cs
@@ -5,40 +5,63 @@ using UnityEngine.UI;
 
 public class FPSCounter : MonoBehaviour {
 
+	public bool showFPS = true;
+	public bool showDT = true;
+
 	// interval between fps checks given in seconds
 	public float updateInterval = 1f;
 
-	// reference to UI label
-	private Text counterLabel;
+	// references to UI labels
+	public Text fpsLabel;
+	public Text dtLabel;
+
 	private int framesSinceLastCheck;
 	private float calculatedFPS;
+	private bool calculateFPSCoroutineRunning;
 
 	void Awake(){
 		framesSinceLastCheck = 0;
 		calculatedFPS = 0;
 	}
 
-	void Start(){
-		counterLabel = transform.GetComponentInChildren<Text> ();
-		if (counterLabel == null) {
-			Debug.LogError ("FPSCounter label not found");
-		} else {
-			InvokeRepeating ("CalculateFPS", 0, updateInterval);
-		}
-	}
-
 	void Update(){
-		framesSinceLastCheck++;
-		UpdateCounterLabel ();
+		if (showFPS) {
+			framesSinceLastCheck++;
+			if (!calculateFPSCoroutineRunning) {
+				StartCoroutine (CalculateFPS ());
+			}
+		}
+		UpdateLabels ();
 	}
 
-	void CalculateFPS(){
-		calculatedFPS = framesSinceLastCheck / updateInterval;
-		framesSinceLastCheck = 0;
+	IEnumerator CalculateFPS(){
+		calculateFPSCoroutineRunning = true;
+		while(showFPS){
+			calculatedFPS = framesSinceLastCheck / updateInterval;
+			framesSinceLastCheck = 0;
+			yield return new WaitForSeconds (updateInterval);
+		}
+		calculateFPSCoroutineRunning = false;
+		yield break;
 	}
 
-	void UpdateCounterLabel(){
-		counterLabel.text = "FPS:" + calculatedFPS.ToString() + " dt:" + Time.deltaTime.ToString();
+	void UpdateLabels(){
+		if (showFPS) {
+			if (!fpsLabel.gameObject.activeSelf) {
+				fpsLabel.gameObject.SetActive (true);
+			}
+			fpsLabel.text = "FPS:" + calculatedFPS.ToString ("0.0");
+		} else {
+			fpsLabel.gameObject.SetActive (false);
+		}
+		if (showDT) {
+			if (!dtLabel.gameObject.activeSelf) {
+				dtLabel.gameObject.SetActive (true);
+			}
+			dtLabel.text = "DT:" + Time.deltaTime.ToString ();
+		} else {
+			dtLabel.gameObject.SetActive (false);
+		}
 	}
 
 }
